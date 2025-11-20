6a1423ea1d71d4ad988b6450d17e853ce9940338
Sakshi Ravindra Darandale <sakshidarandale8@gmail.com>
2025-11-21 00:39:49 +0530

    Add files via upload

---
+++ Java_Programming/ThreadUserDefinedSynchronized.java
@@ -0,0 +1,26 @@
+//Code to demonstrate thread scheduling
+
+class TablePrinter {
+    public void PrintTable(int n) {
+        int i = 0;
+
+        for (i = 1; i <= 10; i++) {
+            System.out.println("Name : " + Thread.currentThread().getName() + " : " + n * i);
+        }
+    }
+}
+
+class Demo extends Thread {
+    public void run() {
+
+    }
+}
+
+class ThreadUserDefinedSynchronized {
+    public static void main(String A[]) {
+        TablePrinter tobj = new TablePrinter();
+        tobj.PrintTable(5);
+        tobj.PrintTable(4);
+    }
+
+}
